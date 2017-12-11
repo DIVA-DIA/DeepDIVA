6ed7f20f379767aa9a015cc1fa639af6fc107acb
Vinaychandran Pondenkandath <vinaychandran.pondenkandath@unifr.ch>
2017-12-11 15:00:08 +0100

    Revert "added: pretrained flag, lr decay, saving models and resuming, logging upon"
    This reverts commit 3b90b06773e20f6e433b1c1b771e975037f19e69.

--- template/standard.py
+++ template/standard.py
@@ -13,7 +13,6 @@ import json
 import os
 import random
 import time
-import shutil
 
 # Tensor board
 import tensorboardX
@@ -49,10 +48,6 @@ parser.add_argument('--dataset',
                     help='one of {CIFAR10, CIFAR100}', default='CIFAR10')
 parser.add_argument('--log-dir',
                     help='where to save logs', default='./data/')
-parser.add_argument('--log-folder',
-                    help='override default log folder (to resume logging of experiment)',
-                    default=None,
-                    type=str)
 
 # Training Options
 parser.add_argument('--init',
@@ -76,12 +71,7 @@ parser.add_argument('--epochs',
 parser.add_argument('--resume',
                     help='path to latest checkpoint',
                     default=None, type=str)
-parser.add_argument('--pretrained',
-                    default=False, action='store_true', help='use pretrained model')
-parser.add_argument('--decay_lr',
-                    default=None, type=int, help='drop LR by 10 every N epochs')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                    help='manual epoch number (useful on restarts)')
+
 # System Options
 parser.add_argument('--gpu-id',
                     default=None,
@@ -122,10 +112,7 @@ if args.seed:
 # Setup Logging
 basename = args.log_dir
 experiment_name = args.experiment_name
-if not args.log_folder:
-    log_folder = os.path.join(basename, experiment_name, '{}'.format(time.strftime('%y-%m-%d-%Hh-%Mm-%Ss')))
-else:
-    log_folder = args.log_folder
+log_folder = os.path.join(basename, experiment_name, '{}'.format(time.strftime('%y-%m-%d-%Hh-%Mm-%Ss')))
 logfile = 'logs.txt'
 if not os.path.exists(log_folder):
     os.makedirs(log_folder)
@@ -140,7 +127,7 @@ logging.info(
 # Save args to logs_folder
 logging.info(
     'Arguments saved to: {}'.format(os.path.join(log_folder, 'args.txt')))
-with open(os.path.join(log_folder, 'args.txt'), 'a') as f:
+with open(os.path.join(log_folder, 'args.txt'), 'w') as f:
     f.write(json.dumps(vars(args)))
 
 # Define Tensorboard SummaryWriter
@@ -168,55 +155,33 @@ def main():
         train_ds = CIFAR10(root='.data/',
                            train=True,
                            download=True,
-                           )
-        val_ds = CIFAR10(root='.data/',
-                         train=False,
-                         val=True,
-                         download=True,
-                         )
+                           transform=transforms.Compose(
+                               # [transforms.Scale((224,224)),transforms.ToTensor()]))
+                               [transforms.ToTensor()]))
         test_ds = CIFAR10(root='.data/',
                           train=False,
                           download=True,
-                          )
+                          transform=transforms.Compose(
+                              # [transforms.Scale((224,224)),transforms.ToTensor()]))
+                              [transforms.ToTensor()]))
         num_outputs = 10
     else:
         train_ds = CIFAR100(root='.data/',
                             train=True,
                             download=True,
-                            )
+                            transform=transforms.Compose(
+                                [transforms.ToTensor()]))
 
         test_ds = CIFAR100(root='.data/',
                            train=False,
                            download=True,
-                           )
+                           transform=transforms.Compose(
+                               [transforms.ToTensor()]))
         num_outputs = 100
-
-    train_ds.transform = transforms.Compose([
-        # transforms.Scale((224, 224)),
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
-    ])
-
-    val_ds.transform = transforms.Compose([
-        # transforms.Scale((224, 224)),
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
-    ])
-
-    test_ds.transform = transforms.Compose([
-        # transforms.Scale((224, 224)),
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
-    ])
 
     # Setup dataloaders
     logging.info('Set up dataloaders')
     train_loader = torch.utils.data.DataLoader(train_ds,
-                                               batch_size=args.batch_size,
-                                               num_workers=args.workers,
-                                               pin_memory=True)
-
-    val_loader = torch.utils.data.DataLoader(val_ds,
                                                batch_size=args.batch_size,
                                                num_workers=args.workers,
                                                pin_memory=True)
@@ -244,57 +209,14 @@ def main():
         criterion = criterion.cuda()
         cudnn.benchmark = True
 
-    if args.resume:
-        if os.path.isfile(args.resume):
-            logging.info("Loading checkpoint '{}'".format(args.resume))
-            checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
-            best_prec1 = checkpoint['best_prec1']
-            model.load_state_dict(checkpoint['state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            val_losses = []
-            val_losses.append(checkpoint['val_loss'])
-            logging.info("Loaded checkpoint '{}' (epoch {})"
-                  .format(args.resume, checkpoint['epoch']))
-        else:
-            logging.info("No checkpoint found at '{}'".format(args.resume))
-
-    else:
-        val_losses = []
-
-
-
-
-
-    # Initialize values for freezeR/rev_freezeR
-    unfreeze_everything = False
-    freeze = args.freeze
-    rev_freeze = args.rev_freeze
-    if freeze:
-        unfrozen_layer = 0
-    else:
-        unfrozen_layer = len(list(list(model.children())[0].children())) - 1
-
-
-
     # Begin training
     logging.info('Begin training')
-    for i in range(args.start_epoch, args.epochs):
+    for i in range(args.epochs):
         # TODO pass the validation loader (if any)
-        val_loss, val_prec1 = validate(val_loader, model, criterion, i)
+        validate(test_loader, model, criterion, i)
         train(train_loader, model, criterion, optimizer, i)
-        if args.decay_lr is not None:
-            adjust_learning_rate(optimizer, i, args.decay_lr)
 
-        is_best = val_prec1 > best_prec1
-        best_prec1 = max(val_prec1, best_prec1)
-        save_checkpoint({
-            'epoch': i + 1,
-            'arch': str(type(model)),
-            'state_dict': model.state_dict(),
-            'best_prec1': best_prec1,
-            'optimizer': optimizer.state_dict(),
-        }, is_best, filename=os.path.join(log_folder, 'checkpoint.pth.tar'))
+
 
     logging.info('Training completed')
 
@@ -464,110 +386,8 @@ def validate(val_loader, model, criterion, epoch):
     print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
           .format(top1=top1, top5=top5))
 
-    return losses.avg, top1.avg
+    return
 
-
-def test(val_loader, model, criterion, epoch):
-    """
-    The validation routine
-    :param val_loader:    torch.utils.data.DataLoader
-        The dataloader of the train set
-    :param model:           torch.nn.module
-        The network model being used
-    :param criterion:       torch.nn.loss
-        The loss function used to compute the loss of the model
-    :param epoch:
-        Number of the epoch (mainly for logging purposes)
-    :return:
-        None
-    """
-
-    # Init the counters
-    batch_time = AverageMeter()
-    losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
-
-    # Switch to evaluate mode (turn off dropout & such )
-    model.eval()
-
-    # Iterate over whole validation set
-    end = time.time()
-    for i, (input, target) in enumerate(val_loader):
-
-        # Moving data to GPU
-        if not args.no_cuda:
-            input = input.cuda(async=True)
-            target = target.cuda(async=True)
-
-        # Convert the input and its labels to Torch Variables
-        input_var = torch.autograd.Variable(input, volatile=True)
-        target_var = torch.autograd.Variable(target, volatile=True)
-
-        # Compute output
-        output = model(input_var)
-
-        # Compute and record the loss
-        loss = criterion(output, target_var)
-        losses.update(loss.data[0], input.size(0))
-
-        # Compute and record the accuracy
-        acc1, acc5 = accuracy(output.data, target, topk=(1, 5))
-        top1.update(acc1[0], input.size(0))
-        top5.update(acc5[0], input.size(0))
-
-        # Add loss and accuracy to Tensorboard
-        writer.add_scalar('test/mb_loss', loss.data[0],
-                          epoch * len(val_loader) + i)
-        writer.add_scalar('test/mb_accuracy', acc1.cpu().numpy(),
-                          epoch * len(val_loader) + i)
-
-        # Measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        if i % args.log_interval == 0:
-            logging.info('Test Epoch [{0}][{1}/{2}]\t'
-                         'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                         'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                         'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                         'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                epoch, i, len(val_loader), batch_time=batch_time, loss=losses,
-                top1=top1, top5=top5))
-
-    # Logging the epoch-wise accuracy
-    writer.add_scalar('test/accuracy', top1.avg, epoch - 1)
-
-    print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-          .format(top1=top1, top5=top5))
-
-    return losses.avg
-
-
-def unfreeze_layer(model, unfrozen_layer):
-    if unfrozen_layer<0 or unfrozen_layer>=len(list(list(model.children())[0].children())):
-        raise Exception
-    for param in model.parameters():
-        param.requires_grad = False
-    # Unfreeze specific layer, list(model.children())[0].children() is used since the model is wrapped in a
-    # DataParallel the wrapper makes it such that model.children() will have only one child, i.e., the whole model.
-    for idx, child in enumerate(list(model.children())[0].children()):
-        if idx == unfrozen_layer:
-            for param in child.parameters():
-                param.requires_grad = True
-    return model
-
-
-def adjust_learning_rate(optimizer, epoch, num_epochs):
-    """Sets the learning rate to the initial LR decayed by 10 every N epochs"""
-    lr = args.lr * (0.1 ** (epoch // num_epochs))
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
-    torch.save(state, filename)
-    if is_best:
-        shutil.copyfile(filename, os.path.join(os.path.split(filename)[0],'model_best.pth.tar'))
 
 if __name__ == "__main__":
     # Set up logging to console
