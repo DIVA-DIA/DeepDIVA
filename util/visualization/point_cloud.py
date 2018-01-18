2cf53a5ff85760c82e3929e672ca68f5ce379bb4
Vinaychandran Pondenkandath <vinaychandran.pondenkandath@unifr.ch>
2018-01-18 13:57:43 +0100

    function to plot the classification grid for a point cloud

---
+++ util/visualization/point_cloud.py
@@ -0,0 +1,84 @@
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import visdom
+import numpy as np
+import logging
+
+
+def plot_to_visdom(grid_x, grid_y, grid_z, point_x, point_y, point_class, num_classes, win_name=None):
+    try:
+        vis = visdom.Visdom()
+    except:
+        logging.error('Visdom Server unavailable')
+    X, Y = grid_x, grid_y
+    zdata = grid_z + 1
+    point_class += 1
+
+    levels = np.linspace(1, num_classes + 1, 1000)
+
+    # Matplotlib stuff
+    fig = plt.figure(1)
+    axs = plt.gca()
+
+    # Plot [RED, GREEN, BLUE, PURPLE, ORANGE]
+    colors_points = ['#b30000', '#009900', '#000099', '#7300e6', '#e68a00']
+    # colors_contour = ['#ff4d4d', '#33ff33', '#4d4dff', '#bf80ff', '#ffcc80']
+    colors_contour = [plt.get_cmap('Reds'), plt.get_cmap('Greens'), plt.get_cmap('Blues'), plt.get_cmap('Purples'),
+                      plt.get_cmap('Oranges')]
+
+    zdata_floor = np.floor(zdata)
+
+    for i in range(1, num_classes + 1):
+        tmp = np.copy(zdata)
+        try:
+            locs = np.where(zdata_floor != i)
+        except ValueError:
+            continue
+            logging.warning("No predictions for class {}".format(i-1))
+        tmp[locs[0], locs[1]] = 0
+        locs = np.where(tmp != 0)
+        vmin, vmax = np.min(tmp[locs[0], locs[1]]), np.max(tmp[locs[0], locs[1]])
+        axs.contourf(X, Y, tmp, levels=levels, cmap=colors_contour[i - 1],vmin=vmin, vmax=vmax)
+
+    for i in range(1, num_classes + 1):
+        locs = np.where(point_class == i)
+        axs.scatter(point_x[locs], point_y[locs], c=colors_points[i - 1], edgecolor='w', lw=0.5)
+
+    # Draw image
+    fig.canvas.draw()
+    # Get image
+    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    # Plot to visdom
+    win_name = vis.image(np.transpose(data, [2, 0, 1]), win=win_name)
+
+    # Get image
+    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+    # Plot to visdom
+    win_name = vis.image(np.transpose(data, [2, 0, 1]), win=win_name)
+
+    return win_name
+
+if __name__ == "__main__":
+    # Make data
+    # x = np.linspace(0, 1, 100)
+    min_x = 0
+    min_y = 0
+    max_x = 1
+    max_y = 1
+    POINTS_RESOLUTION = 100
+    coords = np.array([[x, y] for x, y in zip(np.linspace(min_x, max_x, POINTS_RESOLUTION),
+                                              np.linspace(min_y, max_y, POINTS_RESOLUTION))])
+
+    # X,Y = np.meshgrid(coords, x.T)
+    zdata = np.array([(x, y, np.round(x)) for x in np.linspace(0, 4, 100) for y in np.linspace(0, 1, 100)])
+    zdata = zdata[:, 2].reshape(100, 100) + np.reshape(np.array([y
+                                                                 for x in np.linspace(0, 1, 100)
+                                                                 for y in np.linspace(0, 1, 100)]), (100, 100))
+
+    plot_to_visdom(coords[:, 0], coords[:, 1], zdata, np.array([0.8, 0.6, 0.2, 0.6]), np.array([0.4, 0.6, 0.8, 0.2]),
+                   np.array([0, 0, 1, 1]), 5)
