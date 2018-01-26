096899fb8079ae3b435df454b3af7c33537b3808
Michele Alberti <albertimichele91@hotmail.com>
2018-01-26 18:37:47 +0100

    Partial refactor of setup_dataloaders(). Missing loading CSV mean&std.

--- template/setup.py
+++ template/setup.py
@@ -11,12 +11,11 @@ import torch.nn as nn
 import torch.nn.parallel
 import torch.optim
 import torch.utils.data
-import torchvision
 import torchvision.transforms as transforms
 
 # DeepDIVA
-import datasets
 import models
+from datasets.image_folder_dataset import load_dataset
 from init.initializer import *
 
 
@@ -91,7 +90,7 @@ def set_up_model(num_classes, model_name, pretrained, optimizer_name, lr, no_cud
     return model, criterion, optimizer, best_value, start_epoch
 
 
-def set_up_dataloaders(model_expected_input_size, dataset, dataset_folder, batch_size, workers, **kwargs):
+def set_up_dataloaders(model_expected_input_size, dataset_folder, online, batch_size, workers, **kwargs):
     """
     Set up the dataloaders for the specified datasets.
 
@@ -99,11 +98,14 @@ def set_up_dataloaders(model_expected_input_size, dataset, dataset_folder, batch
     ----------
     :param model_expected_input_size: tuple
         Specify the height and width that the model expects.
-    :param dataset: string
-        NAme of the dataset (might be None)
 
     :param dataset_folder: string
         Path string that points to the three folder train/val/test. Example: ~/../../data/svhn
+
+    :param online: boolean
+        Flag: if True, the dataset is loaded in an online fashion i.e. only file names are stored and images are loaded
+        on demand. This is slower than storing everything in memory.
+
     :param batch_size: int
         Number of datapoints to process at once
 
@@ -117,119 +119,34 @@ def set_up_dataloaders(model_expected_input_size, dataset, dataset_folder, batch
         Three dataloaders for train, val and test. Number of classes for the model.
     """
 
-    logging.info('Loading datasets')
-    # If the dataset selected is a class in dataset, use it.
-    if (dataset is not None) and (dataset in datasets.__dict__):
+    # Recover dataset name
+    dataset = os.path.basename(os.path.normpath(kwargs['dataset_folder']))
+    logging.info('Loading {} from:{}'.format(dataset, dataset_folder))
 
-        logging.debug('Using an user defined class to load: ' + dataset)
-        train_ds = datasets.__dict__[dataset](root='.data/',
-                                              train=True,
-                                              download=True)
+    # Load the dataset splits
+    train_ds, val_ds, test_ds = load_dataset(dataset_folder, online)
 
-        val_ds = datasets.__dict__[dataset](root='.data/',
-                                            train=False,
-                                            val=True,
-                                            download=True)
+    # TODO load the csv, if not yet there: call routine to create csv
 
-        test_ds = datasets.__dict__[dataset](root='.data/',
-                                             train=False,
-                                             download=True)
-    # Else, assume it is an image folder whose path is passed as 'args.dataset_folder'
-    else:
-        logging.debug('Using the image folder routine to load from: ' + dataset_folder)
-        """
-        Structure of the dataset expected
-        
-        Split folders 
-        -------------
-        'args.dataset_folder' has to point to the three folder train/val/test. 
-        Example:  
-        
-        ~/../../data/svhn
-        
-        where the dataset_folder contains the splits sub-folders as follow:
-        
-        args.dataset_folder/train
-        args.dataset_folder/val
-        args.dataset_folder/test
-        
-        Classes folders
-        ---------------
-        In each of the three splits (train,val,test) should have different classes in a separate folder with the class 
-        name. The file name can be arbitrary (e.g does not have to be 0-* for classes 0 of MNIST).
-        Example:
-        
-        train/dog/whatever.png
-        train/dog/you.png
-        train/dog/like.png
-        
-        train/cat/123.png
-        train/cat/nsdf3.png
-        train/cat/asd932_.png
-        """
-
-        # Get the splits folders
-        traindir = os.path.join(dataset_folder, 'train')
-        valdir = os.path.join(dataset_folder, 'test')  # TODO change as soon as svhn has a val set :)
-        testdir = os.path.join(dataset_folder, 'test')
-
-        # Sanity check on the splits folders
-        if not os.path.isdir(traindir):
-            logging.error("Train folder not found in the args.dataset_folder=" + dataset_folder)
-            sys.exit(-1)
-        if not os.path.isdir(valdir):
-            logging.error("Val folder not found in the args.dataset_folder=" + dataset_folder)
-            sys.exit(-1)
-        if not os.path.isdir(testdir):
-            logging.error("Test folder not found in the args.dataset_folder=" + dataset_folder)
-            sys.exit(-1)
-
-        # Init the dataset splits
-        train_ds = torchvision.datasets.ImageFolder(traindir)
-        val_ds = torchvision.datasets.ImageFolder(valdir)
-        test_ds = torchvision.datasets.ImageFolder(testdir)
-
-    # TODO what about the normalization?
-    # train_ds.__getitem__(0) to get an image but its weird?
     # Set up dataset transforms
     logging.debug('Setting up dataset transforms')
-    try:
-        train_ds.transform = transforms.Compose([
-            transforms.Scale(model_expected_input_size),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
-        ])
+    train_ds.transform = transforms.Compose([
+        transforms.Scale(model_expected_input_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
+    ])
 
-        val_ds.transform = transforms.Compose([
-            transforms.Scale(model_expected_input_size),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
-        ])
+    val_ds.transform = transforms.Compose([
+        transforms.Scale(model_expected_input_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
+    ])
 
-        test_ds.transform = transforms.Compose([
-            transforms.Scale(model_expected_input_size),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
-        ])
-    except:
-        logging.warning('Normalization disabled! No mean and std found.')
-        train_ds.transform = transforms.Compose([
-            transforms.Scale(model_expected_input_size),
-            transforms.ToTensor(),
-            # transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
-        ])
-
-        val_ds.transform = transforms.Compose([
-            transforms.Scale(model_expected_input_size),
-            transforms.ToTensor(),
-            # transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
-        ])
-
-        test_ds.transform = transforms.Compose([
-            transforms.Scale(model_expected_input_size),
-            transforms.ToTensor(),
-            # transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
-        ])
+    test_ds.transform = transforms.Compose([
+        transforms.Scale(model_expected_input_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=train_ds.mean, std=train_ds.std)
+    ])
 
     # Setup dataloaders
     logging.debug('Setting up dataloaders')
