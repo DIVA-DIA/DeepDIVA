a92a173c915ef53f8ab5f4e980542593c6143dff
Michele Alberti <albertimichele91@hotmail.com>
2018-02-12 12:32:53 +0100

    Added the initial flatten (required after changes into the pc_ds transforms)

--- models/FC_simple.py
+++ models/FC_simple.py
@@ -31,6 +31,7 @@ class FC_simple(nn.Module):
         :return: torch.Tensor
             Activations of the fully connected layer
         """
+        x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = self.fc2(x)
         return x
