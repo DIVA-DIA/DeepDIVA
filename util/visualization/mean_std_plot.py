acf9fce9bc33e207ebf7fd61536c9bea1658198d
Vinaychandran Pondenkandath <vinaychandran.pondenkandath@unifr.ch>
2018-02-20 15:13:32 +0100

    added min and max curves to the mean-std plot and changed some printed values.

--- util/visualization/mean_std_plot.py
+++ util/visualization/mean_std_plot.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-def plot_mean_variance(arr, suptitle='', title='', xlabel='X', ylabel='Y', ylim=None):
+def plot_mean_std(arr, suptitle='', title='', xlabel='X', ylabel='Y', ylim=None):
     fig = plt.figure(1)
     arr_mean =  np.mean(arr, 0)
     arr_std = np.std(arr, 0)
+    arr_min = np.min(arr, 0)
+    arr_max = np.max(arr, 0)
     with sns.axes_style('darkgrid'):
         fig.suptitle(suptitle)
         plt.title(title)
@@ -16,9 +18,12 @@ def plot_mean_variance(arr, suptitle='', title='', xlabel='X', ylabel='Y', ylim=
             axes.set_ylim(ylim)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        plt.plot(arr_mean, 'b-', label='MeanIU (LDA)')
-        axes.fill_between(np.arange(len(arr_mean)), arr_mean - arr_std, arr_mean + arr_std, color='blue', alpha=0.2)
+        plt.plot(arr_mean, '-', color='#0000b3', label='Score')
+        plt.plot(arr_min, color='#4d4dff', linestyle='dashed', label='Min')
+        plt.plot(arr_max, color='#4d4dff', linestyle='dashed', label='Max')
+        axes.fill_between(np.arange(len(arr_mean)), arr_mean - arr_std, arr_mean + arr_std, color='#9999ff', alpha=0.2)
         plt.legend(loc='best')
+
         fig.canvas.draw()
         data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
         data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
