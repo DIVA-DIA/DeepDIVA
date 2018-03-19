fix to make mean-variance plots begin from -1 for val

template/runner/point_cloud/point_cloud.py
@@ -102,7 +102,7 @@ class PointCloud(Standard):
 
         # Core routine
         logging.info('Begin training')
-        val_value = np.zeros((epochs - start_epoch))
+        val_value = np.zeros((epochs + 1 - start_epoch))
         train_value = np.zeros((epochs - start_epoch))
 
         # Make data for points
@@ -125,7 +125,7 @@ class PointCloud(Standard):
                                             num_classes=num_classes, writer=writer, epoch=-1, epochs=epochs,
                                             **kwargs)
 
-        PointCloud._validate(val_loader, model, criterion, writer, -1, **kwargs)
+        val_value[-1] = PointCloud._validate(val_loader, model, criterion, writer, -1, **kwargs)
         # Add model parameters to Tensorboard
         for name, param in model.named_parameters():
             writer.add_histogram(name, param.clone().cpu().data.numpy(), -1, bins='auto')

template/runner/standard/standard.py
@@ -70,10 +70,10 @@ class Standard:
 
         # Core routine
         logging.info('Begin training')
-        val_value = np.zeros((epochs - start_epoch))
+        val_value = np.zeros((epochs + 1 - start_epoch))
         train_value = np.zeros((epochs - start_epoch))
 
-        Standard._validate(val_loader, model, criterion, writer, -1, **kwargs)
+        val_value[0] = Standard._validate(val_loader, model, criterion, writer, -1, **kwargs)
         for epoch in range(start_epoch, epochs):
             # Train
             train_value[epoch] = Standard._train(train_loader, model, criterion, optimizer, writer, epoch, **kwargs)

template/setup.py
@@ -73,7 +73,7 @@ def set_up_model(num_classes, model_name, pretrained, optimizer_name, lr, no_cud
     """
     # Initialize the model
     logging.info('Setting up model {}'.format(model_name))
-    model = models.__dict__[model_name](num_classes=num_classes, pretrained=pretrained)
+    model = models.__dict__[model_name](output_channels=num_classes, pretrained=pretrained)
     optimizer = torch.optim.__dict__[optimizer_name](model.parameters(), lr)
     if disable_databalancing:
         criterion = nn.CrossEntropyLoss()