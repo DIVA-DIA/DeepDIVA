894e531a1357150ee7693c6825b7bfce0d0ca421
vinaychandranp <vinaychandranp@gmail.com>
2018-03-24 22:02:30 +0100

    new torchvision transform to generate multiple random crops out of a single image.

---
+++ template/runner/triplet/transforms.py
@@ -0,0 +1,76 @@
+import torch
+import numbers
+import numpy as np
+
+
+class MultiCrop(object):
+    """Crop the given PIL Image into multiple random crops
+
+    .. Note::
+         This transform returns a tuple of images and there may be a mismatch in the number of
+         inputs and targets your Dataset returns. See below for an example of how to deal with
+         this.
+
+    Args:
+         size (sequence or int): Desired output size of the crop. If size is an ``int``
+            instead of sequence like (h, w), a square crop of size (size, size) is made.
+
+    Example:
+         >>> transform = Compose([
+         >>>    FiveCrop(size), # this is a list of PIL Images
+         >>>    Lambda(lambda crops: torch.stack([ToTensor()(crop) for crop in crops])) # returns a 4D tensor
+         >>> ])
+         >>> #In your test loop you can do the following:
+         >>> input, target = batch # input is a 5d tensor, target is 2d
+         >>> bs, ncrops, c, h, w = input.size()
+         >>> result = model(input.view(-1, c, h, w)) # fuse batch size and ncrops
+         >>> result_avg = result.view(bs, ncrops, -1).mean(1) # avg over crops
+    """
+
+    def __init__(self, size, n_crops):
+        self.size = size
+        self.n_crops = n_crops
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            assert len(size) == 2, "Please provide only two dimensions (h, w) for size."
+            self.size = size
+
+    def __call__(self, img):
+        return multi_crop(img, self.size, self.n_crops)
+
+
+def multi_crop(img, size, n_crops):
+    """Crop the given PIL Image into multiple random crops.
+
+    .. Note::
+        This transform returns a tuple of images and there may be a
+        mismatch in the number of inputs and targets your ``Dataset`` returns.
+
+    Args:
+       size (sequence or int): Desired output size of the crop. If size is an
+           int instead of sequence like (h, w), a square crop (size, size) is
+           made.
+    Returns:
+        tuple: tuple (tl, tr, bl, br, center) corresponding top left,
+            top right, bottom left, bottom right and center crop.
+    """
+    if isinstance(size, numbers.Number):
+        size = (int(size), int(size))
+    else:
+        assert len(size) == 2, "Please provide only two dimensions (h, w) for size."
+
+    w, h = img.size
+    crop_h, crop_w = size
+    if crop_w > w or crop_h > h:
+        raise ValueError("Requested crop size {} is bigger than input size {}".format(size,
+                                                                                      (h, w)))
+
+    crops = []
+    for i in range(n_crops):
+        x = np.random.randint(0, w - crop_w)
+        y = np.random.randint(0, h - crop_h)
+        assert x + crop_w < w
+        assert y + crop_h < h
+        crops.append(img.crop((x, y, x + crop_w, y + crop_h)))
+    return crops
