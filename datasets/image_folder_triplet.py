0f6ee616000d8524c8699fe987639122febd7acc
vinaychandranp <vinaychandranp@gmail.com>
2018-04-14 19:43:30 +0200

    consolidated inmem functionality into ImageFolderTriplet and made it compatible with mAP evaluation

--- datasets/image_folder_triplet.py
+++ datasets/image_folder_triplet.py
@@ -91,40 +91,21 @@ def load_dataset(dataset_folder, inmem=False, workers=1, num_triplets=None, mode
         logging.error("Test folder not found in the args.dataset_folder=" + dataset_folder)
         sys.exit(-1)
 
-    if inmem:
-        logging.info('Loading the datasets into memory')
-        train_ds = ImageFolderTripletInMem(train_dir,
-                                           train=True,
-                                           num_triplets=num_triplets,
-                                           workers=workers,
-                                           model_expected_input_size=model_expected_input_size)
-        val_ds = ImageFolderTripletInMem(val_dir,
-                                         train=False,
-                                         num_triplets=num_triplets,
-                                         workers=workers,
-                                         model_expected_input_size=model_expected_input_size)
-        test_ds = ImageFolderTripletInMem(test_dir,
-                                          train=False,
-                                          num_triplets=num_triplets,
-                                          workers=workers,
-                                          model_expected_input_size=model_expected_input_size)
-        return train_ds, val_ds, test_ds
-    else:
-        train_ds = ImageFolderTriplet(train_dir, train=True, num_triplets=num_triplets)
-        val_ds = ImageFolderTriplet(val_dir, train=False, num_triplets=num_triplets)
-        test_ds = ImageFolderTriplet(test_dir, train=False, num_triplets=num_triplets)
-        return train_ds, val_ds, test_ds
-
-    return
+    train_ds = ImageFolderTriplet(train_dir, train=True, num_triplets=num_triplets, workers=workers, inmem=inmem)
+    val_ds = ImageFolderTriplet(val_dir, train=False, num_triplets=num_triplets, workers=workers, inmem=inmem)
+    test_ds = ImageFolderTriplet(test_dir, train=False, num_triplets=num_triplets, workers=workers, inmem=inmem)
+    return train_ds, val_ds, test_ds
 
 
 class ImageFolderTriplet(data.Dataset):
 
-    def __init__(self, dataset_folder, train=None, num_triplets=None, transform=None, target_transform=None):
+    def __init__(self, dataset_folder, train=None, num_triplets=None, transform=None, target_transform=None, inmem=None, workers=None):
         self.dataset_folder = os.path.expanduser(dataset_folder)
         self.train = train
         self.transform = transform
         self.target_transform = target_transform
+        self.num_triplets = num_triplets
+        self.inmem = inmem
 
         dataset = torchvision.datasets.ImageFolder(dataset_folder)
 
@@ -140,42 +121,18 @@ class ImageFolderTriplet(data.Dataset):
 
         if self.train:
             self.triplets = self.generate_triplets(self.labels, num_triplets)
-        else:
-            self.matches = self.generate_matches(self.labels, num_triplets)
 
-    def generate_matches(self, labels, num_triplets):
-        """
-        matches has format [anchor, positive/negative, match/not_match]; match = 1, not_match=0
-        """
-        logging.info('Begin generating matches')
-        matches = []
-        end = int(num_triplets / 2)
-        for i in trange(num_triplets):
-            if i < end:
-                c1 = np.random.randint(0, np.max(labels))
-                c1_items = np.where(labels == c1)[0]
-                a = random.choice(c1_items)
-                p = random.choice(c1_items)
-                while a == p:
-                    p = random.choice(c1_items)
-                matches.append([a, p, 1])
-            else:
-                c1 = np.random.randint(0, np.max(labels))
-                c2 = np.random.randint(0, np.max(labels))
-                while c1 == c2:
-                    c2 = np.random.randint(0, np.max(labels))
-                c1_items = np.where(labels == c1)[0]
-                c2_items = np.where(labels == c2)[0]
-                a = random.choice(c1_items)
-                n = random.choice(c2_items)
-                matches.append([a, n, 0])
-        assert len(matches) == num_triplets
-        return matches
+        if self.inmem:
+            # Load all samples
+            with Pool(workers) as pool:
+                self.data = pool.map(self._load_into_mem_and_resize, self.file_names)
 
-    def generate_triplets(self, labels, num_triplets):
+    def generate_triplets(self):
         """
-        triplets has format [anchor, positive, negative]
+        triplets have format [anchor, positive, negative]
         """
+        labels = self.labels
+        num_triplets = self.num_triplets
         logging.info('Begin generating triplets')
         triplets = []
         for i in trange(num_triplets):
@@ -198,97 +155,6 @@ class ImageFolderTriplet(data.Dataset):
     def _load_into_mem(self, path):
         return cv2.imread(path)
 
-    def __getitem__(self, index):
-        """
-        Parameters:
-        -----------
-
-        :param index : int
-            Index of the sample
-
-        :return: tuple:
-            (image, target) where target is index of the target class.
-        """
-        # TODO: Make it parameterized to use TOPN or FPR type data returns
-        if not self.train:
-            a, pn, l = self.matches[index]
-            img_a = Image.fromarray(self._load_into_mem(self.file_names[a]))
-            img_pn = Image.fromarray(self._load_into_mem(self.file_names[pn]))
-            if self.transform is not None:
-                img_a = self.transform(img_a)
-                img_pn = self.transform(img_pn)
-            return img_a, img_pn, l
-
-        # if not self.train:
-        #     # a, pn, l = self.matches[index]
-        #     l = self.labels[index]
-        #     img_a = self._load_into_mem(self.file_names[index])
-        #     img_a = Image.fromarray(img_a)
-        #     if self.transform is not None:
-        #         img_a = self.transform(img_a)
-        #     return img_a, l
-
-        a, p, n = self.triplets[index]
-
-        # Doing this so that it is consistent with all other datasets to return a PIL Image
-        img_a = Image.fromarray(self._load_into_mem(self.file_names[a]))
-        img_p = Image.fromarray(self._load_into_mem(self.file_names[p]))
-        img_n = Image.fromarray(self._load_into_mem(self.file_names[n]))
-
-        if self.transform is not None:
-            img_a = self.transform(img_a)
-            img_p = self.transform(img_p)
-            img_n = self.transform(img_n)
-
-        return img_a, img_p, img_n
-
-    def __len__(self):
-        if self.train:
-            return len(self.triplets)
-        else:
-            return len(self.matches)
-
-
-class ImageFolderTripletInMem(ImageFolderTriplet):
-    """
-    This class makes use of torchvision.datasets.ImageFolder() to create an online dataset.
-    Afterward all images are sequentially stored in memory for faster use when paired with dataloders.
-    It is responsibility of the user ensuring the dataset actually fits in memory.
-    """
-
-    def __init__(self, dataset_folder, train=None, num_triplets=None, transform=None, target_transform=None, workers=1,
-                 model_expected_input_size=(224, 224)):
-
-        self.dataset_folder = os.path.expanduser(dataset_folder)
-        self.transform = transform
-        self.target_transform = target_transform
-        self.model_expected_input_size = model_expected_input_size
-
-        # Get an online dataset
-        dataset = torchvision.datasets.ImageFolder(dataset_folder)
-
-        # Shuffle the data once (otherwise you get clusters of samples of same class in each minibatch for val and test)
-        np.random.shuffle(dataset.imgs)
-
-        # Extract the actual file names and labels as entries
-        self.file_names = np.asarray([item[0] for item in dataset.imgs])
-        self.labels = np.asarray([item[1] for item in dataset.imgs])
-
-        # Load all samples
-        pool = Pool(workers)
-        self.data = pool.map(self._load_into_mem_and_resize, self.file_names)
-        pool.close()
-
-        # Set expected class attributes
-        self.classes = np.unique(self.labels)
-
-        self.train = train
-
-        if self.train:
-            self.triplets = self.generate_triplets(self.labels, num_triplets)
-        else:
-            self.matches = self.generate_matches(self.labels, int(num_triplets / 10))
-
     def _load_into_mem_and_resize(self, path):
         """
         Load an image, resize it into the expected size for the model and convert to PIL image.
@@ -296,7 +162,6 @@ class ImageFolderTripletInMem(ImageFolderTriplet):
         :return:
         """
         img = self._load_into_mem(path)
-        # img = cv2.resize(img, None, fx=0.5, fy=0.5)
         return img
 
     def __getitem__(self, index):
@@ -310,38 +175,29 @@ class ImageFolderTripletInMem(ImageFolderTriplet):
         :return: tuple:
             (image, target) where target is index of the target class.
         """
-
-        # TODO: Make it parameterized to use TOPN or FPR type data returns
         if not self.train:
-            a, pn, l = self.matches[index]
-            img_a = self.data[a]
-            img_pn = self.data[pn]
-            img_a = Image.fromarray(img_a)
-            img_pn = Image.fromarray(img_pn)
+            # a, pn, l = self.matches[index]
+            l = self.labels[index]
+            if self.inmem:
+                img_a = self.data[index]
+            else:
+                img_a = self._load_into_mem(self.file_names[index])
+                img_a = Image.fromarray(img_a)
             if self.transform is not None:
                 img_a = self.transform(img_a)
-                img_pn = self.transform(img_pn)
-            return img_a, img_pn, l
-
-        # if not self.train:
-        #     # a, pn, l = self.matches[index]
-        #     l = self.labels[index]
-        #     img_a = self.data[index]
-        #     img_a = Image.fromarray(img_a)
-        #     if self.transform is not None:
-        #         img_a = self.transform(img_a)
-        #     return img_a, l
+            return img_a, l
 
         a, p, n = self.triplets[index]
 
         # Doing this so that it is consistent with all other datasets to return a PIL Image
-        img_a = self.data[a]
-        img_p = self.data[p]
-        img_n = self.data[n]
-
-        img_a = Image.fromarray(img_a)
-        img_p = Image.fromarray(img_p)
-        img_n = Image.fromarray(img_n)
+        if self.inmem:
+            img_a = Image.fromarray(self.data[a])
+            img_p = Image.fromarray(self.data[p])
+            img_n = Image.fromarray(self.data[n])
+        else:
+            img_a = Image.fromarray(self._load_into_mem(self.file_names[a]))
+            img_p = Image.fromarray(self._load_into_mem(self.file_names[p]))
+            img_n = Image.fromarray(self._load_into_mem(self.file_names[n]))
 
         if self.transform is not None:
             img_a = self.transform(img_a)
