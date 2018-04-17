d53957ebc6c34a3c56f2ffd4b8ee379806be36e9
Michele Alberti <michele.alberti@unifr.ch>
2018-04-17 17:26:05 +0200

    Docstring refactored and added when missing

--- models/TNet.py
+++ models/TNet.py
@@ -1,21 +1,43 @@
-"""
-Tfeat network from  http://www.iis.ee.ic.ac.uk/%7Evbalnt/shallow_descr/TFeat_paper.pdf
-"""
+
 import torch.nn as nn
 
 
 class Flatten(nn.Module):
+    """
+    Flatten a convolution block into a simple vector.
+
+    Replaces the flattening line (view) often found into forward() methods of networks. This makes it
+    easier to navigate the network with introspection
+    """
     def forward(self, x):
         x = x.view(x.size()[0], -1)
         return x
 
 
 class TNet(nn.Module):
-    """
-    TFeat model definition
+    r"""
+    Tfeat network from http://www.iis.ee.ic.ac.uk/%7Evbalnt/shallow_descr/TFeat_paper.pdf
+
+    Attributes
+    ----------
+    conv1 : torch.nn.Conv2d
+    conv2 : torch.nn.Conv2d
+        Convolutional layers of the network
+    fc : torch.nn.Linear
+        Final classification fully connected layer
     """
 
     def __init__(self, output_channels=128, input_channels=3, **kwargs):
+        """
+        Creates an TNet model from the scratch.
+
+        Parameters
+        ----------
+        output_channels : int
+            Number of neurons in the last layer
+        input_channels : int
+            Dimensionality of the input, typically 3 for RGB
+        """
         super(TNet, self).__init__()
 
         self.expected_input_size = (32, 32)
@@ -40,9 +62,15 @@ class TNet(nn.Module):
     def forward(self, x):
         """
         Computes forward pass on the network
-        :param x: torch.Tensor
-            The input to the model
-        :return: torch.Tensor
+
+        Parameters
+        ----------
+        x : Variable
+            Sample to run forward pass on. (input to the model)
+
+        Returns
+        -------
+        Variable
             Activations of the fully connected layer
         """
         x = self.conv1(x)
--- models/_AlexNet.py
+++ models/_AlexNet.py
@@ -3,14 +3,51 @@ import logging
 import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
 
-model_urls = {
-    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
-}
+model_urls = {'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth', }
 
 
-class AlexNet(nn.Module):
+class Flatten(nn.Module):
+    """
+    Flatten a convolution block into a simple vector.
+
+    Replaces the flattening line (view) often found into forward() methods of networks. This makes it
+    easier to navigate the network with introspection
+    """
+
+    def forward(self, x):
+        x = x.view(x.size()[0], -1)
+        return x
+
+
+class _AlexNet(nn.Module):
+    r"""
+    AlexNet model architecture from the `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+
+    Attributes
+    ----------
+    conv1 : torch.nn.Conv2d
+    conv2 : torch.nn.Conv2d
+    conv3 : torch.nn.Conv2d
+    conv4 : torch.nn.Conv2d
+    conv5 : torch.nn.Conv2d
+        Convolutional layers of the network
+    fc1 : torch.nn.Linear
+    fc2 : torch.nn.Linear
+        Fully connected layer
+    cl : torch.nn.Linear
+        Final classification fully connected layer
+    """
+
     def __init__(self, output_channels=1000):
-        super(AlexNet, self).__init__()
+        """
+        Creates an AlexNet model from the scratch.
+
+        Parameters
+        ----------
+        output_channels : int
+            Number of neurons in the last layer
+        """
+        super(_AlexNet, self).__init__()
 
         self.expected_input_size = (227, 227)
 
@@ -18,57 +55,65 @@ class AlexNet(nn.Module):
         self.conv1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
-            # nn.Softsign(),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
         self.conv2 = nn.Sequential(
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
-            # nn.Softsign()
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
         self.conv3 = nn.Sequential(
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            # nn.Softsign(),
         )
         self.conv4 = nn.Sequential(
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            # nn.Softsign()
         )
+
         self.conv5 = nn.Sequential(
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            # nn.Softsign(),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Dropout2d()
         )
 
         # Fully connected layers
         self.fc1 = nn.Sequential(
+            Flatten(),
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
-            # nn.Softsign(),
             nn.Dropout(),
         )
         self.fc2 = nn.Sequential(
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            # nn.Softsign(),
         )
+
         # Classification layer
         self.cl = nn.Sequential(
             nn.Linear(4096, output_channels),
         )
 
     def forward(self, x):
+        """
+        Computes forward pass on the network
+
+        Parameters
+        ----------
+        x : Variable
+            Sample to run forward pass on. (input to the model)
+
+        Returns
+        -------
+        Variable
+            Activations of the fully connected layer
+        """
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
         x = self.conv4(x)
         x = self.conv5(x)
-        x = x.view(x.size(0), 256 * 6 * 6)
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.cl(x)
@@ -76,12 +121,15 @@ class AlexNet(nn.Module):
 
 
 def alexnet(pretrained=False, **kwargs):
-    r"""AlexNet model architecture from the
-    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    model = AlexNet(**kwargs)
+    Returns an AlexNet model, possibly ImageNet pretrained.
+
+    Parameters
+    ----------
+    pretrained : bool
+        If True, returns a model pre-trained on ImageNet
+    """
+    model = _AlexNet(**kwargs)
     if pretrained:
         try:
             model.load_state_dict(model_zoo.load_url(model_urls['alexnet']), strict=False)
