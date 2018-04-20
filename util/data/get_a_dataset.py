5632fbd9074cd62ede2e00c240cbd5672904bd07
vinaychandranp <vinaychandranp@gmail.com>
2018-04-20 15:26:49 +0200

    script to download and prepare toy datasets

---
+++ util/data/get_a_dataset.py
@@ -0,0 +1,83 @@
+"""
+This script allows for creation of a validation set from the training set.
+
+"""
+
+# Utils
+import argparse
+import os
+import shutil
+import inspect
+import sys
+import logging
+
+import torch
+import torchvision
+import numpy as np
+from PIL import Image
+from util.data.dataset_splitter import split_dataset
+
+
+def mnist(args):
+    # Use torchvision to download the dataset
+    torchvision.datasets.MNIST(root=args.output_folder, download=True)
+
+    # Load the data into memory
+    train_data, train_labels = torch.load(os.path.join(args.output_folder,
+                                                       'processed',
+                                                       'training.pt'))
+    test_data, test_labels = torch.load(os.path.join(args.output_folder,
+                                                     'processed',
+                                                     'test.pt'))
+
+    # Make output folders
+    dataset_root = os.path.join(args.output_folder, 'MNIST')
+    train_folder = os.path.join(dataset_root, 'train')
+    test_folder = os.path.join(dataset_root, 'test')
+
+    _make_folder_if_not_exists(dataset_root)
+    _make_folder_if_not_exists(train_folder)
+    _make_folder_if_not_exists(test_folder)
+
+    def _write_data_to_folder(arr, labels, folder):
+        for i, (img, label) in enumerate(zip(arr, labels)):
+            dest = os.path.join(folder, str(label))
+            _make_folder_if_not_exists(dest)
+            Image.fromarray(img.numpy(), mode='L').save(os.path.join(dest, str(i) + '.png'))
+
+    # Write the images to the folders
+    _write_data_to_folder(train_data, train_labels, train_folder)
+    _write_data_to_folder(test_data, test_labels, test_folder)
+
+    shutil.rmtree(os.path.join(args.output_folder, 'raw'))
+    shutil.rmtree(os.path.join(args.output_folder, 'processed'))
+
+    split_dataset(dataset_folder=dataset_root, split=0.2, symbolic=False)
+
+    return
+
+
+def _make_folder_if_not_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+if __name__ == "__main__":
+    downloadable_datasets = [name[0] for name in inspect.getmembers(sys.modules[__name__],
+                                                                    inspect.isfunction) if not name[0].startswith('_')]
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description='This script can be used to download some '
+                                                 'datasets and prepare them in a standard format')
+
+    parser.add_argument('dataset',
+                        help='name of the dataset',
+                        type=str,
+                        choices=downloadable_datasets)
+    parser.add_argument('--output-folder',
+                        help='path to where the dataset should be generated.',
+                        required=False,
+                        type=str,
+                        default='./data/')
+    args = parser.parse_args()
+
+    getattr(sys.modules[__name__], args.dataset)(args)
