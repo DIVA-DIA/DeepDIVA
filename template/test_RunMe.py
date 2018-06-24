2dff9181bb338d8ece0ed05dfc17e40769ec33d6
Michele Alberti <michele.alberti@unifr.ch>
2018-06-24 22:21:10 +0200

    placeholder for tests added

---
+++ template/test_RunMe.py
@@ -0,0 +1,16 @@
+"""
+Warning: LONG RUNTIME TESTS!
+
+This test suite is designed to verify that the main components of the framework are not broken.
+It is expected that smaller components or sub-parts are tested individually.
+
+As we all know this will probably never happen, we will at least verify that the overall features
+are correct and fully functional. These tests will take long time to run and are not supposed to
+be run frequently. Nevertheless, it is important that before a PR or a push on the master branch
+the main functions can be tested.
+
+Please keep the list of these tests up to date as soon as you add new features.
+"""
+
+def test_one():
+    pass
