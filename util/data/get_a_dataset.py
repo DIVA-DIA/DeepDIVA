aaab0f0db3228e6e7e003ca8fb26523eb20680f3
vinaychandranp <vinaychandranp@gmail.com>
2018-07-22 12:40:08 +0200

    added possibility to get SVHN dataset

--- util/data/get_a_dataset.py
+++ util/data/get_a_dataset.py
@@ -4,11 +4,13 @@ import inspect
 import os
 import shutil
 import sys
-from PIL import Image
 
+import numpy as np
+import scipy
 # Torch
 import torch
 import torchvision
+from PIL import Image
 
 # DeepDIVA
 from util.data.dataset_splitter import split_dataset
@@ -16,7 +18,7 @@ from util.data.dataset_splitter import split_dataset
 
 def mnist(args):
     """
-    Fetches and prepare in a DeepDIVA friendly format the MNIST dataset on the location specified
+    Fetches and prepares (in a DeepDIVA friendly format) the MNIST dataset to the location specified
     on the file system
 
     Parameters
@@ -61,6 +63,63 @@ def mnist(args):
 
     shutil.rmtree(os.path.join(args.output_folder, 'raw'))
     shutil.rmtree(os.path.join(args.output_folder, 'processed'))
+
+    split_dataset(dataset_folder=dataset_root, split=0.2, symbolic=False)
+
+
+def svhn(args):
+    """
+    Fetches and prepares (in a DeepDIVA friendly format) the SVHN dataset to the location specified
+    on the file system
+
+    Parameters
+    ----------
+    args : dict
+        List of arguments necessary to run this routine. In particular its necessary to provide
+        output_folder as String containing the path where the dataset will be downloaded
+
+    Returns
+    -------
+        None
+    """
+    # Use torchvision to download the dataset
+    torchvision.datasets.SVHN(root=args.output_folder, split='train', download=True)
+    torchvision.datasets.SVHN(root=args.output_folder, split='test', download=True)
+
+    # Load the data into memory
+    train = scipy.io.loadmat(os.path.join(args.output_folder,
+                                          'train_32x32.mat'))
+    train_data, train_labels = train['X'], train['y'].astype(np.int64).squeeze()
+    np.place(train_labels, train_labels == 10, 0)
+    train_data = np.transpose(train_data, (3, 0, 1, 2))
+
+    test = scipy.io.loadmat(os.path.join(args.output_folder,
+                                         'test_32x32.mat'))
+    test_data, test_labels = test['X'], test['y'].astype(np.int64).squeeze()
+    np.place(test_labels, test_labels == 10, 0)
+    test_data = np.transpose(test_data, (3, 0, 1, 2))
+
+    # Make output folders
+    dataset_root = os.path.join(args.output_folder, 'SVHN')
+    train_folder = os.path.join(dataset_root, 'train')
+    test_folder = os.path.join(dataset_root, 'test')
+
+    _make_folder_if_not_exists(dataset_root)
+    _make_folder_if_not_exists(train_folder)
+    _make_folder_if_not_exists(test_folder)
+
+    def _write_data_to_folder(arr, labels, folder):
+        for i, (img, label) in enumerate(zip(arr, labels)):
+            dest = os.path.join(folder, str(label))
+            _make_folder_if_not_exists(dest)
+            Image.fromarray(img).save(os.path.join(dest, str(i) + '.png'))
+
+    # Write the images to the folders
+    _write_data_to_folder(train_data, train_labels, train_folder)
+    _write_data_to_folder(test_data, test_labels, test_folder)
+
+    os.remove(os.path.join(args.output_folder, 'train_32x32.mat'))
+    os.remove(os.path.join(args.output_folder, 'test_32x32.mat'))
 
     split_dataset(dataset_folder=dataset_root, split=0.2, symbolic=False)
 
