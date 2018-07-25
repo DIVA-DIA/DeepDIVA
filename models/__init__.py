8e8e14c3689d429249efb38a33c56d0c00e1cac8
vinaychandranp <vinaychandranp@gmail.com>
2018-07-25 13:44:31 +0200

    added DenseNet, Inception_v3, VGG into built-in models

--- models/__init__.py
+++ models/__init__.py
@@ -3,9 +3,12 @@ from .CNN_basic import CNN_basic
 from .FC_medium import FC_medium
 from .FC_simple import FC_simple
 from .TNet import TNet
-from ._AlexNet import _AlexNet, alexnet
+from ._AlexNet import alexnet
 from ._ResNet import resnet18, resnet34, resnet50, resnet101, resnet152
 from .BabyResNet import babyresnet18, babyresnet34, babyresnet50, babyresnet101, babyresnet152
+from ._VGG import vgg11, vgg11_bn, vgg13, vgg13_bn, vgg16, vgg16_bn, vgg19, vgg19_bn
+from ._Inception_v3 import inception_v3
+from ._DenseNet import densenet121, densenet161, densenet169, densenet201
 
 """
 Formula to compute the output size of a conv. layer
