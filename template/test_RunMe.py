5793d62719c3f0a4d01cf904c11052f4b301c269
Michele Alberti <michele.alberti@unifr.ch>
2018-08-05 23:25:46 +0200

    prototype of test batch (unfinished)

--- template/test_RunMe.py
+++ template/test_RunMe.py
@@ -11,6 +11,36 @@ the main functions can be tested.
 
 Please keep the list of these tests up to date as soon as you add new features.
 """
+import numpy as np
+
+from template.RunMe import RunMe
 
 def test_one():
-    pass
+    """
+    - Verify the sizes of the return of execute
+    - Image classification with default parameters
+    """
+    epochs = 1
+    args = ["--experiment-name", "test_template",
+            "--ignoregit",
+            "--dataset-folder", "/dataset/cifar",
+            "--epochs", str(epochs)]
+
+    train, val, test = RunMe().main(args=args)
+
+    # Verify sizes of the return values from execute
+    assert len(train.shape) == 2
+    assert train.shape[0] == 1
+    assert train.shape[1] == epochs
+
+    assert len(val.shape) == 2
+    assert train.shape[0] == 1
+    assert train.shape[1] == epochs+1
+
+    assert len(train.shape) == 1
+    assert train.shape[0] == 1
+
+    # Verify values
+    np.testing.assert_almost_equal(train[0], 100.0)
+    np.testing.assert_almost_equal(val[0], 100.0)
+    np.testing.assert_almost_equal(test, 100.0)
