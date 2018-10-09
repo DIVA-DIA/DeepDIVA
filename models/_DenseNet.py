424a484d222bdc53ef92956b2dc59a5b64aa8677
vinaychandranp <vinaychandranp@gmail.com>
2018-10-09 16:43:57 +0200

    added kwargs to propogate the arguments to the model

--- models/_DenseNet.py
+++ models/_DenseNet.py
@@ -140,7 +140,7 @@ class DenseNet(nn.Module):
     """
 
     def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
-                 num_init_features=64, bn_size=4, drop_rate=0, output_channels=1000):
+                 num_init_features=64, bn_size=4, drop_rate=0, output_channels=1000, **kwargs):
 
         super(DenseNet, self).__init__()
 
