3c9778586c01dacffe5bcc3b94d42b8dc22ea5ea
vinaychandranp <vinaychandranp@gmail.com>
2018-10-31 11:35:03 +0100

    added multi-label multi-instance image dataset (MIML) dataset getter.

--- util/data/get_a_dataset.py
+++ util/data/get_a_dataset.py
@@ -4,9 +4,13 @@ import inspect
 import os
 import shutil
 import sys
+import wget
+import rarfile
 
+from scipy.io import loadmat as _loadmat
 import numpy as np
-import scipy
+import pandas as pd
+from sklearn.model_selection import train_test_split as _train_test_split
 # Torch
 import torch
 import torchvision
@@ -14,6 +18,8 @@ from PIL import Image
 
 # DeepDIVA
 from util.data.dataset_splitter import split_dataset
+from util.misc import get_all_files_in_folders_and_subfolders \
+    as _get_all_files_in_folders_and_subfolders
 
 
 def mnist(args):
@@ -171,6 +177,97 @@ def cifar10(args):
     shutil.rmtree(os.path.join(args.output_folder, 'cifar-10-batches-py'))
 
     split_dataset(dataset_folder=dataset_root, split=0.2, symbolic=False)
+
+def miml(args):
+    """
+    Fetches and prepares (in a DeepDIVA friendly format) the Multi-Instance Multi-Label Image Dataset
+    on the file system. Dataset available at: http://lamda.nju.edu.cn/data_MIMLimage.ashx
+
+    Parameters
+    ----------
+    args : dict
+        List of arguments necessary to run this routine. In particular its necessary to provide
+        output_folder as String containing the path where the dataset will be downloaded
+
+    Returns
+    -------
+        None
+    """
+    # Download the files
+    url = 'http://lamda.nju.edu.cn/files/miml-image-data.rar'
+    if not os.path.exists(os.path.join(args.output_folder, 'miml-image-data.rar')):
+        print('Downloading file!')
+        filename = wget.download(url, out=args.output_folder)
+    else:
+        print('File already downloaded!')
+        filename = os.path.join(args.output_folder, 'miml-image-data.rar')
+
+    # Extract the files
+    path_to_rar = filename
+    path_to_output = os.path.join(args.output_folder, 'tmp_miml')
+    rarfile.RarFile(path_to_rar).extractall(path_to_output)
+    path_to_rar = os.path.join(path_to_output, 'original.rar')
+    rarfile.RarFile(path_to_rar).extractall(path_to_output)
+    path_to_rar = os.path.join(path_to_output, 'processed.rar')
+    rarfile.RarFile(path_to_rar).extractall(path_to_output)
+    print('Extracted files...')
+
+    # Load the mat file
+    mat = _loadmat(os.path.join(path_to_output, 'miml data.mat'))
+    targets = mat['targets'].T
+    classes = [item[0][0] for item in mat['class_name']]
+    # Add filename at 0-index to correctly format the CSV headers
+    classes.insert(0, 'filename')
+
+    # Get list of all image files in the folder
+    images = [item for item in _get_all_files_in_folders_and_subfolders(path_to_output)
+                if item.endswith('jpg')]
+    images = sorted(images, key=lambda e: int(os.path.basename(e).split('.')[0]))
+
+
+
+    # Make splits
+    train_data, test_data, train_labels, test_labels = _train_test_split(images, targets, test_size=0.2, random_state=42)
+    train_data, val_data, train_labels, val_labels = _train_test_split(train_data, train_labels, test_size=0.2, random_state=42)
+
+    # print('Size of splits\ntrain:{}\nval:{}\ntest:{}'.format(len(train_data),
+    #                                                     len(val_data),
+    #                                                     len(test_data)))
+
+    # Make output folders
+    dataset_root = os.path.join(args.output_folder, 'MIML')
+    train_folder = os.path.join(dataset_root, 'train')
+    val_folder = os.path.join(dataset_root, 'val')
+    test_folder = os.path.join(dataset_root, 'test')
+
+    _make_folder_if_not_exists(dataset_root)
+    _make_folder_if_not_exists(train_folder)
+    _make_folder_if_not_exists(val_folder)
+    _make_folder_if_not_exists(test_folder)
+
+
+    def _write_data_to_folder(data, labels, folder, classes):
+        for image, label in zip(data, labels):
+            dest = folder
+            shutil.copy(image, folder)
+
+        rows = np.column_stack(([os.path.basename(item) for item in data], labels))
+        rows = sorted(rows, key=lambda e: int(e[0].split('.')[0]))
+        output_csv = pd.DataFrame(rows)
+        output_csv.to_csv(os.path.join(folder, 'labels.csv'), header=classes, index=False)
+        return
+
+    # Write the images to the correct folders
+    print('Writing the data to the filesystem')
+    _write_data_to_folder(train_data, train_labels, train_folder, classes)
+    _write_data_to_folder(val_data, val_labels, val_folder, classes)
+    _write_data_to_folder(test_data, test_labels, test_folder, classes)
+
+    os.remove(filename)
+    shutil.rmtree(path_to_output)
+    print('All done!')
+    return
+
 
 
 def _make_folder_if_not_exists(path):
