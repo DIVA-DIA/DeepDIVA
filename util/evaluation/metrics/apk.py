fixed a bug in mapk computation

util/evaluation/metrics/apk.py
@@ -33,23 +33,24 @@ def apk(query, predicted, k='full'):
         Average Precision@k
 
     """
-    if k == 'auto':
-        k = predicted.count(query)
-    elif k == 'full':
-        k = len(predicted)
+    assert (len(predicted) > 0)
 
-    if k == 0 or len(predicted) == 0:
+    # Count the number of relevant items that could be retrieved
+    num_hits = predicted.count(query)
+    if num_hits == 0:
         return 0
 
-    if len(predicted) > k:
-        predicted = predicted[:k]
+    # Resolve k in case is not a number
+    if k == 'auto':
+        k = num_hits
+    elif k == 'full':
+        k = len(predicted)
+    else:
+        assert isinstance(k, int)
+    assert (k > 0) and (k <= len(predicted))
 
-    predicted = np.array(predicted)
-
-    num_hits = len(np.where(predicted == query)[0])
-
-    # Divide by min(k, num_hits)
-    k_or_num_hits = min(k, num_hits)
+    # Truncate the list to the number of desired elements which gets taken into account
+    predicted = np.array(predicted[:k])
 
     # Non-vectorized version.
     # score = 0.0  # The score is the precision@i integrated over i=1:k
@@ -78,7 +79,7 @@ def apk(query, predicted, k='full'):
     # Divide element-wise by [0/1,1/2,0/3,2/4,0/5,3/6] and sum the array.
     score = np.sum(np.divide(relevant, np.arange(1, relevant.shape[0] + 1)))
 
-    return score / k_or_num_hits
+    return score / min(k, num_hits)
 
 
 def mapk(query, predicted, k=None, workers=1):

util/evaluation/metrics/test_apk.py
@@ -1,30 +1,39 @@
 import numpy as np
+import pytest
 
 from util.evaluation.metrics import apk, mapk
 
 
-def test_accuracy():
-    # Sanity check
-    assert apk(1, []) == 0.0
-
+def test_sanity_check():
     # Test k > |prediction|
-    assert apk(5, [5, 4, 3, 2, 1, 0], 8) == 0.125
-    assert apk(5, [5, 4, 3, 2, 1, 0], 10) == 0.1
+    with pytest.raises(AssertionError):
+        apk(1, [1, 2, 3], 8)
 
+    # Test k == 0
+    with pytest.raises(AssertionError):
+        apk(1, [1, 2, 3], 0)
+
+    # Test k non int
+    with pytest.raises(AssertionError):
+        apk(1, [1, 2, 3], 'hello')
+
+    # Test []
+    with pytest.raises(AssertionError):
+        apk(1, [])
 
 def test_apk_base_case():
     # Test base base case scenario
     assert apk(1, [0, 0]) == 0.0
-    assert apk(1, [0, 1]) == 0.25
-    assert apk(1, [1, 0]) == 0.5
+    assert apk(1, [0, 1]) == 0.5
+    assert apk(1, [1, 0]) == 1.0
     assert apk(1, [1, 1]) == 1.0
 
 
 def test_apk_full():
     # Test non-zero labels & 'full'
     assert apk(1, [2, 3], 'full') == 0.0
-    assert apk(1, [4, 1], 'full') == 0.25
-    assert apk(1, [1, 5], 'full') == 0.5
+    assert apk(1, [4, 1], 'full') == 0.5
+    assert apk(1, [1, 5], 'full') == 1.0
     assert apk(1, [1, 1], 'full') == 1.0
 
 
@@ -46,17 +55,16 @@ def test_apk_auto():
 
 def test_apk_advanced():
     # Test with more than 2 numbers
-    assert apk(1, [0, 1, 0, 1]) == 0.25
+    assert apk(1, [0, 1, 0, 1]) == 0.5
     assert apk(1, [0, 1, 0, 1], 2) == 0.25
     assert apk(1, [0, 1, 0, 1], 'auto') == 0.25
 
     assert apk(1, [2, 3, 1, 1], 1) == 0.0
-    assert apk(1, [4, 5, 1, 1], 2) == 0.0
-    assert apk(1, [6, 7, 1, 1], 'auto') == 0.0
-    np.testing.assert_almost_equal([apk(1, [2, 3, 1, 1], 3)], [0.11111111])
-    np.testing.assert_almost_equal([apk(1, [5, 4, 1, 1], 4)], [0.20833333])
-    np.testing.assert_almost_equal([apk(1, [6, 6, 1, 1])], [0.20833333])
-    np.testing.assert_almost_equal([apk(1, [11, 12, 1, 1], 'full')], [0.20833333])
+    assert apk(1, [2, 3, 1, 1], 2) == 0.0
+    assert apk(1, [2, 3, 1, 1], 'auto') == 0.0
+    np.testing.assert_almost_equal([apk(1, [2, 3, 1, 1], 3)], [0.16666666])
+    np.testing.assert_almost_equal([apk(1, [2, 3, 1, 1], 4)], [0.41666666])
+    np.testing.assert_almost_equal([apk(1, [2, 3, 1, 1], 'full')], [0.41666666])
 
 
 def test_apk_long_sequences():
@@ -66,7 +74,7 @@ def test_apk_long_sequences():
 
 def test_mapk():
     # Single entry
-    np.testing.assert_almost_equal([mapk([1], [[2, 3, 1, 1]], 'full')[0]], [0.20833333])
+    np.testing.assert_almost_equal([mapk([1], [[2, 3, 1, 1]], 'full')[0]], [0.41666666])
 
     # Multiple entries
-    np.testing.assert_almost_equal([mapk([1, 1], [[2, 3, 1, 1], [1, 1]], 'full')[0]], [0.60416666])
+    np.testing.assert_almost_equal([mapk([1, 1], [[2, 3, 1, 1], [1, 1]], 'full')[0]], [0.70833333])