c3203ba429099dda614dd17bdc5e95140749dfdc
Renthal <albertimichele91@hotmail.com>
2019-05-29 19:02:55 +0200

    refactor + bugfix

--- datasets/image_folder_segmentation.py
+++ datasets/image_folder_segmentation.py
@@ -10,6 +10,7 @@ import sys
 import math
 import os.path
 from collections import deque
+import numpy as np
 
 # from DeepDIVA
 from template.setup import _load_class_encodings
@@ -155,7 +156,7 @@ def load_dataset(dataset_folder, workers, in_memory=False, **kwargs):
     train_ds = ImageFolder(train_dir, classes, workers, **kwargs)
     val_ds = ImageFolder(val_dir, classes, workers, **kwargs)
     # the number of workers has to be 1 during testing (concurrency issues)
-    test_ds = ImageFolder(test_dir, classes, workers=1, **kwargs)
+    test_ds = ImageFolder(test_dir, classes, num_workers=1, is_test=True, **kwargs)
     return train_ds, val_ds, test_ds
 
 
@@ -169,103 +170,100 @@ class ImageFolder(data.Dataset):
         root/data/xxx.png
         root/data/xxy.png
         root/data/xxz.png
-
-    Args:
-        root (string): Root directory path.
-        transform (callable, optional): A function/transform that  takes in an PIL image
-            and returns a transformed version. E.g, ``transforms.RandomCrop``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-        loader (callable, optional): A function to load an image given its path.
-
-     Attributes:
-        classes (list): List of the class names.
-        class_to_idx (dict): Dict with items (class_name, class_index).
-        imgs (list): List of (image path, class_index) tuples
     """
 
-    def __init__(self, root, classes, workers, imgs_in_memory, crops_per_image, crop_size,
-                 transform=None, img_transform=None, gt_transform=None, loader=default_loader, **kwargs):
+    def __init__(self, root, class_encodings, num_workers, imgs_in_memory, crops_per_image, crop_size,
+                 transform=None, img_transform=None, gt_transform=None, loader=default_loader,
+                 is_test=False, **kwargs):
+        """
+        #TODO doc
+        Parameters
+        ----------
+        root : string
+            Path to dataset folder (train / val / test)
+        class_encodings :
+        num_workers : int
+        imgs_in_memory :
+        crops_per_image : int
+        crop_size : int
+        transform : callable
+        img_transform : callable
+        gt_transform : callable # TODO why are there 3 transforms?
+        loader : callable
+            A function to load an image given its path.
+        """
 
-        img_paths = get_gt_data_paths(root)
-        # the total number of crops needs to be divisible by the number of workers
-        self.num_workers = workers
-        self.loader = loader
-
-        # only used for debugging
-        self.updated = 0
-        self.img_and_updates = {os.path.basename(name[0]): 0 for name in img_paths}
-        self.img_and_num_crops = {os.path.basename(name[0]): 0 for name in img_paths}
-
-        if len(img_paths) == 0:
-            raise (RuntimeError("Found 0 images in subfolders of: " + root + "\nSupported image extensions are: "
-                                + ",".join(IMG_EXTENSIONS)))
-
-        # path to dataset folder (train / val / test)
+        # Init list
         self.root = root
-        # list of tuples that contain the path to the gt and image that belong together
-        self.img_paths = img_paths
-        self.num_imgs_in_set = len(self.img_paths)
-
-        # true if it is the test set
-        self.is_test = "test" == os.path.basename(self.root)
-
-        # transformations and loader
+        self.class_encodings = class_encodings
+        self.num_classes = len(self.class_encodings)
+        self.num_workers = num_workers
+        self.imgs_in_memory = imgs_in_memory
+        self.crops_per_image = crops_per_image if not is_test else None
+        self.crop_size = crop_size
         self.transform = transform
         self.img_transform = img_transform
         self.gt_transform = gt_transform
+        self.loader = loader
+        self.is_test = is_test
 
-        # class encoding (list)
-        self.class_encodings = classes
-        self.num_classes = len(self.class_encodings)
+        # List of tuples that contain the path to the gt and image that belong together
+        self.img_paths = get_gt_data_paths(root)
+        self.num_imgs_in_set = len(self.img_paths)
+        if self.num_imgs_in_set == 0:
+            raise RuntimeError("Found 0 images in subfolders of: {} \n Supported image extensions are: {}".format(
+                root, ",".join(IMG_EXTENSIONS)))
 
-        # set up iterators and parameters
-        self.imgs_in_memory = imgs_in_memory
-        self.crop_size = crop_size
+        # Make sure work can be split into workers equally
+        if self.num_workers > 1:
+            # Length is divisible by the number of workers
+            if self.__len__() % self.num_workers != 0:
+                logging.error("{} (number of pages in set ({}) * crops per image {}) "
+                              "must be divisible by the number of workers (currently {})".format(
+                    self.__len__(), self.num_imgs_in_set, self.crops_per_image, self.num_workers))
+                sys.exit(-1)
+            # Crops per page is divisible by the number of workers
+            if self.crops_per_image % self.num_workers != 0:
+                logging.error("{} (# crops per page) must be divisible by the number of"
+                              " workers (currently {})".format(self.crops_per_image, self.num_workers))
+                sys.exit(-1)
 
-        # index in self.image_order of the next image to be loaded
+        # Index in self.image_order of the next image to be loaded
         self.next_image_index = 0
 
-        # keeping track of how many crops have been generated -> needed to know when to shuffle the pages
-        self.number_of_crops = 0  # total number per worker (__len__ / num_workers)
+        # Keeping track of how many crops have been generated -> needed to know when to shuffle the pages
         self.current_number_of_crops = 0  # set to zero again when new page / page-bundle is loaded
 
         if self.is_test:
-            # overlap for the sliding window (% of crop size)
+            # Overlap for the sliding window (% of crop size)
             self.overlap = 0.5
-            # get the numbers for __len__
+            # Get the numbers for __len__
             self.img_names_sizes, self.num_horiz_crops, self.num_vert_crops = self._get_img_size_and_crop_numbers()
-
         else:
-            self.crops_per_image = crops_per_image
             self.crops_per_image_per_worker = crops_per_image // self.num_workers
-            # list with the index order of the images
-            self.image_order = [i for i in range(self.num_imgs_in_set)]
+            # List with the index order of the images
+            self.image_order = list(range(self.num_imgs_in_set))
 
-        # make sure length is divisible by the number of workers
-        if self.num_workers > 1:
-            if not self.__len__() % self.num_workers == 0:
-                logging.error("{} (number of pages in set ({}) * crops per image {}) must be divisible by the number of workers (currently {})".format(self.__len__(), self.num_imgs_in_set, self.crops_per_image, self.num_workers))
-                sys.exit(-1)
-            if not self.crops_per_image % self.num_workers == 0:
-                logging.error("{} (# crops per page) must be divisible by the number of workers (currently {})".format(self.crops_per_image, self.num_workers))
-                sys.exit(-1)
+        # Only used for debugging
+        self.updated = 0
+        self.img_and_updates = {os.path.basename(name[0]): 0 for name in self.img_paths}
+        self.img_and_num_crops = {os.path.basename(name[0]): 0 for name in self.img_paths}
 
     def __len__(self):
         """
-        This function returns the length of an epoch so the data loader knows when to stop. The length is different during
-        train / val or test, because we process the whole image during testing, and only sample from the images during
-        train / val.
-
+        This function returns the length of an epoch so the data loader knows when to stop.
+        The length is different during train/val and test, because we process the whole image during testing,
+        and only sample from the images during train/val.
         """
         if self.is_test:
-            # sliding window
+            # Sliding window
             return sum([hc*vc for hc, vc in zip(self.num_horiz_crops, self.num_vert_crops)])
         else:
-            # number of images returned for random cropping
+            # Number of images in the dataset * how many crops per page
             return len(self.img_paths) * self.crops_per_image
 
     def __getitem__(self, index):
+        # TODO documentation format
         """
         Args:
             index (int): Index
@@ -278,56 +276,46 @@ class ImageFolder(data.Dataset):
             tuple: ((img_input, orig_img_shape, top_left_coordinates_of_crop, test_img_name), gt_target)
                 default sliding window (used during test)
         """
-        if not self.is_test:
-            # training / validation
-            # shuffle page order at start of epoch
-            if self.number_of_crops == 0:
-                self._shuffle_img_order()
-                # initiate the lists where the images in memory are stored
-                self._load_memory()
-
-            return self._get_train_val_items()
-
-        else:
-            # testing: sliding window with overlap
+        if self.is_test:
+            # Testing: sliding window with overlap
             if self.current_number_of_crops == 0:
                 # load page
                 self._load_test_images_and_vars()
-
             return self._get_test_items()
+        else:
+            return self._get_train_val_items()
 
     def _get_train_val_items(self):
-        if self.current_number_of_crops == 0:
-            # check if new page needs to be loaded
-            if self.number_of_crops > 0 and self.number_of_crops % self._bundle_len() == 0:
-                self._load_memory()
-            # shuffle the image bundle order at the beginning and when a new page is loaded
-            self._shuffle_img_order_bundle()
+        # TODO documentation
+        # New bundle?
+        if self.current_number_of_crops % self._bundle_len() == 0:
+            self.current_number_of_crops = 0
+            # Load new page
+            self._load_page()
+            # Shuffle the image bundle order at the beginning and when a new page is loaded
+            random.shuffle(self.image_bundle_order)
+
+        logging.debug("PID{}: Image order [{}]: {}".format(os.getpid(), len(self.image_order), self.image_order))
+        logging.debug("PID{}: Image bundle order [{}]: {}".format(os.getpid(), self._bundle_len(), self.image_bundle_order))
+        logging.debug("PID{}: Current number of crops: {}".format(os.getpid(), self.current_number_of_crops))
 
         current_img = self.imgnames_inmem[self.image_bundle_order[self.current_number_of_crops]]
-        self.img_and_num_crops[current_img] = self.img_and_num_crops[current_img] + 1
-        logging.debug("PID{}: Image order: {}".format(os.getpid(), self.image_order))
-        logging.debug("PID{}: Image bundle order: {}".format(os.getpid(), self.image_bundle_order))
         logging.debug("PID{}: Cropping from image: {}".format(os.getpid(), current_img))
 
+        self.img_and_num_crops[current_img] = self.img_and_num_crops[current_img] + 1
         logging.debug("**********{}: crops/img {}".format(os.getpid(), self.img_and_num_crops))
 
         # get the items
         img, gt = self.apply_transformation(self.data_img_inmem[self.image_bundle_order[self.current_number_of_crops]],
                                             self.gt_img_inmem[self.image_bundle_order[self.current_number_of_crops]])
 
-        logging.debug("PID{}: Crop number {} / {} of the bundle, {} / {} total".format(os.getpid(),
-                     self.current_number_of_crops+1, self._bundle_len(), self.number_of_crops+1, self._worker_len()))
-
-        # update total number of crops
-        # set to zero when last crop of image bundle is generated
-        self.current_number_of_crops = (self.current_number_of_crops + 1) % (self._bundle_len())
-        # set to zero when last crop of epoch is generated
-        self.number_of_crops = (self.number_of_crops + 1) % (self._worker_len())
+        # Update total number of crops
+        self.current_number_of_crops += 1
 
         return img, gt
 
     def _get_test_items(self):
+        # TODO documentation
         # get and update the coordinates for the sliding window
         coordinates = self._get_crop_coordinates()
         x_position, y_position = coordinates
@@ -346,6 +334,7 @@ class ImageFolder(data.Dataset):
         return (img, coordinates, self.img_names_sizes[self.next_image_index-1][0]), gt
 
     def _load_test_images_and_vars(self):
+        # TODO documentation
         # load image
         self.current_data_img = pil_loader(self.img_paths[self.next_image_index][0])
         self.current_gt_img = pil_loader(self.img_paths[self.next_image_index][1])
@@ -398,18 +387,13 @@ class ImageFolder(data.Dataset):
         return img, gt
 
     def _bundle_len(self):
-        return len(self.image_bundle_order)
+        return self.crops_per_image_per_worker * np.min([len(self.img_paths), self.imgs_in_memory])
 
     def _worker_len(self):
         return int(self.crops_per_image_per_worker * self.num_imgs_in_set)
 
-    def _shuffle_img_order(self):
-        random.shuffle(self.image_order)
-
-    def _shuffle_img_order_bundle(self):
-        random.shuffle(self.image_bundle_order)
-
-    def _load_memory(self):
+    def _load_page(self):
+        # TODO documentation
         # get path to images to load
         to_load = [self.img_paths[i] for i in self.image_order[self.next_image_index:(self.imgs_in_memory + self.next_image_index)]]
 
@@ -420,9 +404,9 @@ class ImageFolder(data.Dataset):
         # update index of next image
         self.next_image_index += self.imgs_in_memory
         logging.debug("PID{}: Current images: {}".format(os.getpid(),
-            [os.path.basename(self.img_paths[i][0]) for i in self.image_order[(self.next_image_index-self.imgs_in_memory):self.next_image_index]]))
+                                                         [os.path.basename(self.img_paths[i][0]) for i in self.image_order[(self.next_image_index-self.imgs_in_memory):self.next_image_index]]))
         # set back to zero if we get outside of the image range
-        if self.next_image_index > self.num_imgs_in_set:
+        if self.next_image_index >= self.num_imgs_in_set:
             self.next_image_index = 0
 
         # gt and data images are the same size
@@ -430,22 +414,28 @@ class ImageFolder(data.Dataset):
             assert img.size == gt.size
 
         # create the order in which the crops are sample from the loaded images
-        self.image_bundle_order = [i for i in range(len(self.data_img_inmem))] * self.crops_per_image_per_worker
+        self.image_bundle_order = list(range(len(self.data_img_inmem))) * self.crops_per_image_per_worker
+
+        # TODO remove
+        # if len(self.image_bundle_order) != self._bundle_len():
+        #     logging.debug("{} = {} * {}".format(len(self.image_bundle_order), len(self.data_img_inmem), self.crops_per_image_per_worker))
+        #     logging.debug("{} = {} * {}".format(self._bundle_len(), self.crops_per_image_per_worker,  np.min([len(self.img_paths), self.imgs_in_memory])))
+        #     sys.exit(-1)
 
         for (data_path, _) in to_load:
             self.img_and_updates[os.path.basename(data_path)] = self.img_and_updates[os.path.basename(data_path)] + 1
         logging.debug("**********{}: page updates {}".format(os.getpid(), self.img_and_updates))
 
     def _get_img_size_and_crop_numbers(self):
+        # TODO documentation
         img_names_sizes = [] # list of tuples -> (gt_img_name, img_size (H, W))
         num_horiz_crops = []
         num_vert_crops = []
 
-        # load image
         for img_path, gt_path in self.img_paths:
             data_img = self.loader(img_path)
             gt_img = self.loader(gt_path)
-            # ensure that data and gt image are of the same size
+            # Ensure that data and gt image are of the same size
             assert gt_img.size == data_img.size
             img_names_sizes.append((os.path.basename(gt_path), data_img.size[::-1]))
             num_horiz_crops.append(math.ceil(data_img.size[0] / (self.crop_size / (1 / self.overlap))))
@@ -454,12 +444,14 @@ class ImageFolder(data.Dataset):
         return img_names_sizes, num_horiz_crops, num_vert_crops
 
     def _update_sliding_window_coordinates(self):
+        # TODO documentation
         self.current_horiz_crop = (self.current_horiz_crop + 1) % self.current_num_horiz_crops
 
         if self.current_horiz_crop == 0 or self.current_vert_crop == 0:
             self.current_vert_crop = (self.current_vert_crop + 1)
 
     def _get_crop_coordinates(self):
+        # TODO documentation
         # x coordinate
         if self.current_horiz_crop == (self.current_num_horiz_crops - 1):
             # we are at the end of a line
