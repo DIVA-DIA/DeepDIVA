a5acd04640aadae654ed6476f8e7b3eea4b13e49
Michele Alberti <michele.alberti@unifr.ch>
2019-05-30 19:39:48 +0200

    refined the OOP paradigm

--- template/runner/divahisdb_semantic_segmentation/divahisdb_semantic_segmentation.py
+++ template/runner/divahisdb_semantic_segmentation/divahisdb_semantic_segmentation.py
@@ -1,10 +1,3 @@
-"""
-This file is the template for the boilerplate of train/test of a DNN for image classification
-
-There are a lot of parameter which can be specified to modify the behaviour and they should be used 
-instead of hard-coding stuff.
-"""
-
 # Utils
 
 # Delegated
@@ -22,33 +15,7 @@ class DivahisdbSemanticSegmentation(ImageClassification):
     @classmethod
     def prepare(cls, model_name, **kwargs):
         """
-        Loads and prepares the data, the optimizer and the criterion
-
-        Parameters
-        ----------
-        model_name : str
-            Name of the model. Used for loading the model.
-        kwargs : dict
-            Any additional arguments.
-
-        Returns
-        -------
-        model : DataParallel
-            The model to train
-        num_classes : int
-            How many different classes there are in our problem. Used for loading the model.
-        best_value : float
-            Best value of the model so far. Non-zero only in case of --resume being used
-        train_loader : torch.utils.data.dataloader.DataLoader
-            Training dataloader
-        val_loader : torch.utils.data.dataloader.DataLoader
-            Validation dataloader
-        test_loader : torch.utils.data.dataloader.DataLoader
-            Test set dataloader
-        optimizer : torch.optim
-            Optimizer to use during training, e.g. SGD
-        criterion : torch.nn.modules.loss
-            Loss function to use, e.g. cross-entropy
+        See parent class for documentation
         """
         # Setting up the dataloaders
         train_loader, val_loader, test_loader = set_up_dataloaders(**kwargs)
@@ -62,11 +29,6 @@ class DivahisdbSemanticSegmentation(ImageClassification):
         return model, len(cls.class_encoding), best_value, train_loader, val_loader, test_loader, optimizer, criterion
 
     ####################################################################################################################
-    """
-    These methods delegate their function to other classes in this package. 
-    It is useful because sub-classes can selectively change the logic of certain parts only.
-    """
-
     @classmethod
     def _train(cls, **kwargs):
         return train.train(class_encodings=cls.class_encoding, **kwargs)
